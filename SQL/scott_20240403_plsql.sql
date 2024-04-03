0a2810e4bd65cb29cbadae6989d6d50496c9af15
ejkim9090 <ej.kh.kim@gmail.com>
2024-04-03 15:07:30 +0900

    procedure 객체 만들기-실행

--- SQL/scott_20240403_plsql.sql
+++ SQL/scott_20240403_plsql.sql
@@ -66,12 +66,112 @@ begin
 --    end loop;
 
     dbms_output.put_line(v_idx);
---    for i in 0..v_idx-1 loop
---        
---    end loop;
+--    for i in 0..v_idx-1 `loop
+    for i in 1..v_idx loop
+        dbms_output.put_line(v_arr_no(i) );
+    end loop;
     
 end;
 /
+DECLARE
+    AAA_UK EXCEPTION;
+    PRAGMA EXCEPTION_INIT(AAA_UK, -00001);
+    BBB_OVERFLOW EXCEPTION;
+    PRAGMA EXCEPTION_INIT(BBB_OVERFLOW, -01438 );
+BEGIN
+    UPDATE emp 
+    SET empno = 7369
+    WHERE empno = 7788;
+EXCEPTION
+    WHEN AAA_UK
+    THEN DBMS_OUTPUT.PUT_LINE('이미 존재하는 사번입니다.');
+    WHEN BBB_OVERFLOW
+    THEN DBMS_OUTPUT.PUT_LINE('4자리 숫자입력해주세요. 자릿수보다 큰 값은 허용되지 않습니다.');
+END;
+/
+--    UPDATE emp 
+--    SET empno = 73691111
+--    WHERE empno = 7788;
+----    ORA-00001: 무결성 제약 조건(SCOTT.PK_EMP)에 위배됩니다
+----ORA-01438: 이 열에 대해 지정된 전체 자릿수보다 큰 값이 허용됩니다.
+----ORA-12899: "SCOTT"."T8"."C1" 열에 대한 값이 너무 큼(실제: 12, 최대값: 10)
+--select * from emp;
+
+create or replace procedure proc_test2
+is
+    AAA_UK EXCEPTION;
+    PRAGMA EXCEPTION_INIT(AAA_UK, -00001);
+    BBB_OVERFLOW EXCEPTION;
+    PRAGMA EXCEPTION_INIT(BBB_OVERFLOW, -01438);
+BEGIN
+    UPDATE emp 
+    SET empno = 7369
+    WHERE empno = 7788;
+EXCEPTION
+    WHEN AAA_UK
+    THEN DBMS_OUTPUT.PUT_LINE('이미 존재하는 사번입니다.');
+    WHEN BBB_OVERFLOW
+    THEN DBMS_OUTPUT.PUT_LINE('자릿수보다 큰 값은 허용되지 않습니다. 4자리 숫자입력해주세요.');
+END;
+/
+
+exec proc_test1;
+
+--select * from user_sequences;
+--select * from user_tables;
+--select * from user_source;
+
+--SELECT_EMPID(200, :VAR_EMP_NAME, :VAR_SALARY, :VAR_BONUS);
+create or replace procedure pro_select_empno ( 
+      v_empno   in    emp.empno%type, 
+      v_ename   out   emp.ename%type, 
+      v_sal     out   emp.sal%type, 
+      v_comm    out   emp.comm%type  
+)
+is
+begin
+    select  ename, sal, comm
+    into v_ename, v_sal, v_comm
+    from emp
+    where empno = v_empno;
+end;
+/
+
+variable a_name varchar2(100);
+variable a_sal number;
+variable a_comm number;
+exec pro_select_empno(7788, :a_name, :a_sal, :a_comm);
+print a_name;
+print a_sal;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
