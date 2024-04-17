7316148365753bb7f79f53757bc99c3e52e47832
ejkim9090 <ej.kh.kim@gmail.com>
2024-04-17 15:50:01 +0900

    mybatis 누락된 설정들
    1. resources 폴더 만들고 오른 마우스 Set Source Folder
    2. windows - preferences - xml(Wild Web Developer) - Download external resources like referenced DTD, XSD 체크하기
    3. window – Preference – Maven – User Settings -Global Settings (open file) 에 ojdbc11.jar 찾아서 지정

---
+++ 31_web_project/src/main/java/kh/mclass/mybatis/common/MybatisTemplate.java
@@ -0,0 +1,23 @@
+package kh.mclass.mybatis.common;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+public class MybatisTemplate {
+	public static SqlSession getSqlSession() {
+		String resource = "mybatis/mybatis-config.xml";
+		SqlSession session = null;   // Connection 역할을 하는 mybatis의 개체
+		try {
+			InputStream inputStream = Resources.getResourceAsStream(resource);
+			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+			session = sqlSessionFactory.openSession();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return session;
+	}
+	
+}
