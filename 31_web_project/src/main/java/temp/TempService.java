ce88dfc9ba6a852d51df9577872d5444265dfc4d
ejkim9090 <ej.kh.kim@gmail.com>
2024-04-19 16:30:00 +0900

    Service, Dao - template 파일  mybatis로 수정

--- 31_web_project/src/main/java/temp/TempService.java
+++ 31_web_project/src/main/java/temp/TempService.java
@@ -1,9 +1,9 @@
 package temp;
 
-
-import static kh.mclass.jdbc.common.JdbcTemplate.*;
-import java.sql.Connection;
+import static kh.mclass.jdbc.common.MybatisTemplate.*;
 import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
 
 import kh.mclass.semim.member.model.dao.MemberDao;
 import kh.mclass.semim.member.model.dto.MemberDto;
@@ -14,41 +14,41 @@ public class TempService {
 	// select list - all
 	public List<MemberDto> selectAllList() {
 		List<MemberDto> result = null;
-		Connection conn = getSemiConnection(true);
-		result = dao.selectAllList(conn);
-		close(conn);
+		SqlSession session = getSqlSession();
+		result = dao.selectAllList(session);
+		session.close();
 		return result;
 	}
 	// select one
 	public MemberDto selectOne(String memId) {
 		MemberDto result = null;
-		Connection conn = getSemiConnection(true);
-		result = dao.selectOne(conn, memId);
-		close(conn);
+		SqlSession session = getSqlSession();
+		result = dao.selectOne(session, memId);
+		session.close();
 		return result;
 	}
 	// insert
 	public int insert(MemberDto dto) {
 		int result = 0;
-		Connection conn = getSemiConnection(true);
-		result = dao.insert(conn, dto);
-		close(conn);
+		SqlSession session = getSqlSession();
+		result = dao.insert(session, dto);
+		session.close();
 		return result;
 	}
 	// update
 	public int update(MemberDto dto) {
 		int result = 0;
-		Connection conn = getSemiConnection(true);
-		result = dao.update(conn, dto);
-		close(conn);
+		SqlSession session = getSqlSession();
+		result = dao.update(session, dto);
+		session.close();
 		return result;
 	}
 	// delete
 	public int delete(String memId) {
 		int result = 0;
-		Connection conn = null;
-		result = dao.delete(conn, memId);
-		close(conn);
+		SqlSession session = getSqlSession();
+		result = dao.delete(session, memId);
+		session.close();
 		return result;
 	}
 }
